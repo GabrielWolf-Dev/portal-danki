96146dd9d25c846a24e48c6acf4765b5a87cca8f
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-02-13 11:57:54 -0300

    Setup inicial do projeto

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+// Setup Body Parser
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies(Form req)
+
+// Setup for read HTML files with 'ejs'
+app.engine('html', ejs.renderFile); // Render files extended .html
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    const searchQuery = req.query.search;
+
+    if(searchQuery === null){
+        res.send('home page!'); 
+    } else {
+        res.send('Você buscou por ' + searchQuery);
+    }
+});
+
+app.get('/:slug', (req, res) => {
+    res.send(req.params.slug);
+});
+
+// Setup run local server
+app.listen('5000', () => {
+    console.log('Server is running :)');
+});
