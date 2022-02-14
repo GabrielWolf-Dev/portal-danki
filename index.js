7020917e4b66535ea15bbe08e06979f9b127dc2c
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-02-13 22:02:55 -0300

    Iniciando a estilização
    Header e começando o slide das notícias do momento

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bo
 app.engine('html', ejs.renderFile); // Render files extended .html
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/pages'));
 
 app.get('/', (req, res) => {
     const searchQuery = req.query.search;
 
-    if(searchQuery === null){
-        res.send('home page!'); 
+    if(searchQuery === undefined){
+        res.render('home', {});
     } else {
         res.send('Você buscou por ' + searchQuery);
     }
