a9ed5c45cd9436311e7b9d54ab2e6a8acf41021f
Gabriel Wolf <gabrielwolf@pop-os.localdomain>
2022-02-24 12:38:29 -0300

    Configurando sessões e a rota pra acessar o admin

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { filterXSS } = require('xss');
+const session = require('express-session');
 
 const app = express();
 const Posts = require('./Posts.js');
+const req = require('express/lib/request');
 
 dotenv.config();
 
 // Connection Mongo Db
-const urlConnect = `mongodb+srv://root:${process.env.PASS_MONGODB}@cluster0.ihfw2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const urlConnect = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_MONGODB}@cluster0.ihfw2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(urlConnect, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,12 +23,18 @@ mongoose.connect(urlConnect, {
     console.log('Conectado!');
 })
 .catch(error => {
-    throw new Error(error.message);
+    console.error(error.message);
 });
 
 // Setup Body Parser
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies(Form req)
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'keyboard cat', // Como é um gerador de keys, para o usuário não acessar os dados da sessão e ser seguro
+    cookie: { maxAge: 60000 } // Definiu a sessão como cookie
+}));
 
 // Setup for read HTML files with 'ejs'
 app.engine('html', ejs.renderFile); // Render files extended .html
@@ -37,7 +45,7 @@ app.set('views', path.join(__dirname, '/pages'));
 app.get('/', (req, res) => {
     const searchQuery = filterXSS(req.query.search);
 
-    if(searchQuery === undefined){
+    if(searchQuery === '' || searchQuery === undefined){
         Posts.find({ })
         .sort({ '_id': -1 })
         .exec((error, posts) => {
@@ -151,6 +159,15 @@ app.get('/:slug', (req, res) => {
             res.redirect('/');
         }
     });
+});
+
+app.get('/admin/login', (req, res) => {
+    if(req.session.login == null) {
+        req.session.login = 'Gabriel';
+        res.send('Sessão criada :)');
+    } else {
+        res.send(req.session.login);
+    }
 });
 
 // Setup run local server
